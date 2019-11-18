c63e7a5f6bb737126e8618eeab75479cc0bde5f2
liuyuhuan12 <55042143+liuyuhuan12@users.noreply.github.com>
2019-11-18 20:27:49 +0800

    第十一周作业“1”
    第十一周作业"1"

---
+++ 第十一周作业/src/第十一周作业/Dx1.java
@@ -0,0 +1,13 @@
+package 第十一周作业;
+
+
+
+public class Dx1 {
+
+	public static void main(String[] args) {
+		Dx2 telcomUser=new Dx2("17395832855");
+		telcomUser.generateCommucationRecord();
+		telcomUser.printDetails();
+	}
+
+}
